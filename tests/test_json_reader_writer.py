2f3304c80c225f408d2acbb17681cdb3a56cd245
Robbie Swager <rswager@ushrauto.com>
2026-06-12 10:17:39 -0400

    Add test for overriding files

--- tests/test_json_reader_writer.py
+++ tests/test_json_reader_writer.py
@@ -67,3 +67,13 @@ def test_read_object_from_file_empty(valid_file_path, empty_dict_entry):
 def test_read_object_from_file_with_missing_file(invalid_missing_file_path, full_dict_entry):
     with pytest.raises(FileNotFoundError):
         read_object_from_file(invalid_missing_file_path)
+
+
+def test_override_file_with_empty_info(valid_file_path, empty_dict_entry, full_dict_entry):
+    write_object_to_file(valid_file_path, full_dict_entry)
+    ret = read_object_from_file(valid_file_path)
+    assert ret == full_dict_entry
+
+    write_object_to_file(valid_file_path, empty_dict_entry)
+    ret = read_object_from_file(valid_file_path)
+    assert ret == empty_dict_entry
